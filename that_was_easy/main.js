7022a2d5e56b767adb592c9b4ada955560c85d37
William <william7901@gmail.com>
2018-01-11 00:17:03 +0000

    Top-Down Shooter Webgame

--- that_was_easy/main.js
+++ that_was_easy/main.js
@@ -1,8 +1,14 @@
-
 function sayThatWasEasy() {
   var thatWasEasy = new Audio("that_was_easy.mp3");
   thatWasEasy.play();
-  alert("somestring");
 }
 
 $("#easy").on("click", sayThatWasEasy);
+
+$(document).keypress(delegateKeypress);
+
+function delegateKeypress(event) {
+  if (event.charCode == 32) { // 32 is space
+    $("#easy").trigger("click");
+  }
+}     
